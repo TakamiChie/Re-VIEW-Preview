206964bc46996e027f9ee007457c6f8fd99298aa
TakamiChie@Visnu <chie@onpu-tamago.net>
2019-03-04 17:11:12 +0900

    Implemented a simple page reload process.

--- html/main.js
+++ html/main.js
@@ -1,3 +1,7 @@
 document.getElementById("review-file").addEventListener("change", function(e) {
   pywebview.api.show_review(e.target.value);
 });
+
+document.getElementById("refresh_view").addEventListener("click", function(e) {
+  pywebview.api.show_review();
+})
