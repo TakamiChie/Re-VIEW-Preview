bf6249b615b92f586633e923163e246ec2841226
TakamiChie@Visnu <chie@onpu-tamago.net>
2021-06-07 20:08:34 +0900

    Classified to ensure extensibility.

--- src/main.py
+++ src/main.py
@@ -11,31 +11,35 @@ import const
 ev = threading.Event()
 api = None
 
-def get_args() -> argparse.Namespace:
-  """
-  Set Arguments
-  """
-  parser = argparse.ArgumentParser()
-  parser.add_argument("dir", help="Directory of Re:VIEW manuscript", type=str, nargs='?')
-  return (parser.parse_args())
+class Application:
 
-def load_thread(window: webview.Window, api) -> None:
-  """
-  Start work thread
-  """
-  api.update_title()
-  api.update_list()
+  def __init__(self) -> None:
+    self.args = self.get_args()
+    self.api = js_api.JSAPI()
+    self.log = logging.getLogger(const.APPNAME)
+    self.log.setLevel(logging.DEBUG)
+    self.log.addHandler(logging.StreamHandler())
 
-def main() -> None:
-  args = get_args()
-  api = js_api.JSAPI()
-  log = logging.getLogger(const.APPNAME)
-  log.setLevel(logging.DEBUG)
-  log.addHandler(logging.StreamHandler())
-  serv = server.Server(logger=log.getChild(server.Server.__name__), api=api)
-  window = webview.create_window(const.APPNAME, serv, width=640, height=320, min_size=(360, 400), js_api=api)
-  api.initialize(args.dir, window)
-  webview.start(load_thread, args=(window, api), debug=True)
+  def get_args(self) -> argparse.Namespace:
+    """
+    Set Arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir", help="Directory of Re:VIEW manuscript", type=str, nargs='?')
+    return (parser.parse_args())
+
+  def load_thread(self, window: webview.Window, api) -> None:
+    """
+    Start work thread
+    """
+    api.update_title()
+    api.update_list()
+
+  def main(self) -> None:
+    self.server = server.Server(logger=self.log.getChild(server.Server.__name__), api=self.api)
+    self.window = webview.create_window(const.APPNAME, self.server, width=640, height=320, min_size=(360, 400), js_api=api)
+    self.api.initialize(self.args.dir, self.window)
+    webview.start(self.load_thread, args=(self.window, self.api), debug=True)
 
 if __name__ == "__main__":
-  main()
+  Application().main()
